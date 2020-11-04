ec3fc4c2ce5105efb40793bc103a594d2c25703b
Shyam Bahety <bahetyshyam@gmail.com>
2020-11-05 00:02:08 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Text>App</Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+
+export default App;
